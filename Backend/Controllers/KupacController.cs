9486d8d4635e77e3a96d65416c5cb8144bef7ac2
Goran Franjic <goran@proces.hr>
2024-03-16 12:44:14 +0100

    dodan faker
    unos kupca

--- Backend/Controllers/KupacController.cs
+++ Backend/Controllers/KupacController.cs
@@ -6,7 +6,7 @@ namespace Backend.Controllers
 {
     [ApiController]
     [Route("api/v1/[controller]")]
-    public class KupacController
+    public class KupacController : ControllerBase
     {
         // Dependency injection
         // Definiraš privatno svojstvo
@@ -23,27 +23,52 @@ namespace Backend.Controllers
         [HttpGet]
         public IActionResult Get()
         {
-            return new JsonResult(_context.Kupci.ToList());
+            try
+            {
+                return new JsonResult(_context.Kupci.ToList());
+            }
+            catch (Exception ex)
+            {
+                return new JsonResult(ex.ToString());
+            }
+
         }
 
         [HttpPost]
-        public IActionResult Post(Kupac smjer)
+        public IActionResult Post(int brojKupaca)
         {
-            _context.Kupci.Add(smjer);
+            for (int i = 0; i < brojKupaca; i++)
+
+            {
+                var noviKupac = new Kupac
+                {
+                    Ime = Faker.Name.First(),
+                    Prezime = Faker.Name.Last(),
+                    Email = Faker.Internet.Email(),
+                    Broj_telefona = Faker.Phone.Number()
+            };
+
+                _context.Kupci.Add(noviKupac);
+
+            }
             _context.SaveChanges();
-            return new JsonResult(smjer);
+            return Ok("Dodani su" + brojKupaca + "novi kupci");
         }
+
+
+
+
 
         [HttpPut]
         [Route("{sifra:int}")]
-        public IActionResult Put(int sifra, Kupac smjer)
+        public IActionResult Put(int sifra, Kupac kupac)
         {
             var smjerIzBaze = _context.Kupci.Find(sifra);
             // za sada ručno, kasnije će doći Mapper
-            smjerIzBaze.Ime = smjer.Ime;
-            smjerIzBaze.Prezime= smjer.Prezime;
-            smjerIzBaze.Email= smjer.Email;
-            smjerIzBaze.Broj_telefona=smjer.Broj_telefona;
+            smjerIzBaze.Ime = kupac.Ime;
+            smjerIzBaze.Prezime = kupac.Prezime;
+            smjerIzBaze.Email = kupac.Email;
+            smjerIzBaze.Broj_telefona = kupac.Broj_telefona;
 
             _context.Kupci.Update(smjerIzBaze);
             _context.SaveChanges();
@@ -59,7 +84,7 @@ namespace Backend.Controllers
             var smjerIzBaze = _context.Kupci.Find(sifra);
             _context.Kupci.Remove(smjerIzBaze);
             _context.SaveChanges();
-            return new JsonResult(new { poruka="Obrisano"});
+            return new JsonResult(new { poruka = "Obrisano" });
         }
 
     }
