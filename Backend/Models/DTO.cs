8b0294727d2e0ca25dad5323358d7488cbfc8fbe
Goran Franjic <goran@proces.hr>
2024-04-18 13:37:58 +0200

    kupac

--- Backend/Models/DTO.cs
+++ Backend/Models/DTO.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Backend.Models
 {
-    public record KupacDTORead(int Id, string Ime, string Prezime, string Email, string BrojTelefona);
+    public record KupacDTORead(int Id, string? Ime, string? Prezime, string? Email, string? BrojTelefona);
     public record KupacDTOInsertUpdate([Required(ErrorMessage = "Ime obavezno")] string? Ime, string? Prezime, string? Email, string? BrojTelefona);
     public record FilmDTORead(int Id, string? Naziv, int? Trajanje, string? Dvorana);
     public record FilmDTOInsertUpdate(
